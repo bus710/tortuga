bad946e37b7c79b484647bf2a4ffaf7115337f62
bus710 <bus710@gmail.com>
2019-08-24 16:02:30 -0700

    wip

---
+++ cmd/tortuga.go
@@ -0,0 +1,11 @@
+package tortuga
+
+import "log"
+
+// Body ...
+type Body struct{}
+
+// Init ...
+func (b *Body) Init() {
+	log.Println("")
+}
---
+++ test/tortuga_test.go
@@ -0,0 +1,11 @@
+package tortuga
+
+import (
+	"https://github.com/bus710/tortuga" tortuga 
+)
+
+func testInit(){
+	body := tortuga.body
+
+	body.init()
+}

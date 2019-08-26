fb85949fd670e077b38db6274d11e8ee31fa6187
bus710 <bus710@gmail.com>
2019-08-25 20:42:58 -0700

    wip

--- tortuga.go
+++ tortuga.go
@@ -8,8 +8,6 @@ import (
 	"time"
 
 	"github.com/tarm/serial"
-
-	"github.com/bus710/tortuga/internal/helper"
 )
 
 // Connection ...
@@ -115,7 +113,7 @@ loopRun:
 			// 	}
 			// 	if data {
 			// 	}
-			helper.Dummy()
+			Dummy()
 		// This case receives a stop signal
 		case <-c.chanStop:
 			break loopRun

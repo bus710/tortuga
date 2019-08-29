f3d06ab1513aef99ab77a6a27c34187f2511ca22
bus710 <bus710@gmail.com>
2019-08-29 12:43:09 -0700

    wip

--- test/tortuga_test.go
+++ test/tortuga_test.go
@@ -1,6 +1,7 @@
 package tortuga
 
 import (
+	"encoding/hex"
 	"log"
 	"sync"
 	"testing"
@@ -13,6 +14,27 @@ import (
 
 type testHelper struct {
 	conn tortuga.Connection
+}
+
+func Test_formatFeedback(t *testing.T) {
+
+	log.Println("Hello!")
+
+	waitInstance := sync.WaitGroup{}
+
+	tHelper := testHelper{}
+	tHelper.conn = tortuga.Connection{}
+	err := tHelper.conn.Init(&waitInstance, tHelper.handler, "ttyUSB0")
+
+	decoded, err := hex.DecodeString("aa554d010f90f1000000ed2b58470d0d00129f00030300000004073c1dfcff0000000506f506f9079006060201010d0e8106a1ff0800c8ff90ff0300cfff10100f00dc0fe00fe00fe00fef0f00000000a100")
+	if err != nil {
+	}
+
+	tHelper.conn.buf = decoded
+
+	tHelper.conn.formatFeedback(0, len(decoded))
+
+	log.Println("Bye")
 }
 
 func Test(t *testing.T) {

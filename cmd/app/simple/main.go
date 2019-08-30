f0a77de3bfe89b3fbde64c80e2d95a8241a0d6cd
bus710 <bus710@gmail.com>
2019-08-30 08:53:38 -0700

    moved the test code to a new simple app

---
+++ cmd/app/simple/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"log"
+	"sync"
+	"time"
+
+	"github.com/bus710/tortuga"
+	"github.com/bus710/tortuga/cmd/command"
+	"github.com/bus710/tortuga/internal/model"
+)
+
+// App to hold/connect variables and methods during the lifecycle
+type App struct {
+	conn tortuga.Connection
+}
+
+func main() {
+
+	log.Println("Hello!")
+
+	waitInstance := sync.WaitGroup{}
+
+	app := App{}
+	app.conn = tortuga.Connection{}
+	err := app.conn.Init(&waitInstance, app.handler, "ttyUSB0")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	waitInstance.Add(1)
+	go app.conn.Run()
+
+	for i := 0; i < 5; i++ {
+		log.Println(i)
+		app.conn.Send(command.BaseControlCommand(50, 0))
+		time.Sleep(time.Second * 1)
+	}
+
+	app.conn.Send(command.BaseControlCommand(0, 0))
+	time.Sleep(time.Second * 2)
+
+	for i := 0; i < 5; i++ {
+		log.Println(i)
+		app.conn.Send(command.BaseControlCommand(-50, 0))
+		time.Sleep(time.Second * 1)
+	}
+
+	app.conn.Send(command.BaseControlCommand(0, 0))
+
+	app.conn.Stop()
+
+	waitInstance.Wait()
+
+	log.Println("Bye!")
+}
+
+func (app *App) handler(feedback model.Feedback) {
+	log.Println()
+	log.Printf("Available Contents: %32b", feedback.AvailableContent)
+	log.Println("0. Time of processing: ", feedback.TimeStamp)
+	log.Println("1. Basic Sensor Data: ", feedback.BasicSensorData)
+	log.Println("3. Docking IR: ", feedback.DockingIR)
+	log.Println("4. Inertial Sensor: ", feedback.InertialSensor)
+	log.Println("5. Cliff: ", feedback.Cliff)
+	log.Println("6. Current: ", feedback.Current)
+	log.Println("10. HW Ver: ", feedback.HardwareVersion)
+	log.Println("11. FW Ver: ", feedback.FirmwareVersion)
+	log.Println("13. Gyro: ", feedback.Gyro.FollowedDataLength/3, feedback.RawGyroDataArray)
+	log.Println("16. GPInput: ", feedback.GeneralPurposeInput)
+	log.Println("19. UDID: ", feedback.UniqueDeviceIdentifier)
+	log.Println("21. Controller Info: ", feedback.ControllerInfo)
+}
--- test/tortuga_test.go
+++ test/tortuga_test.go
@@ -5,10 +5,8 @@ import (
 	"log"
 	"sync"
 	"testing"
-	"time"
 
 	"github.com/bus710/tortuga"
-	"github.com/bus710/tortuga/cmd/command"
 	"github.com/bus710/tortuga/internal/model"
 )
 
@@ -36,47 +34,6 @@ func Test_formatFeedback(t *testing.T) {
 	decoded, _ = hex.DecodeString("aa554d010fb8f1000000fc2b67470d0d00129f00030300000004073e1d4f000000000506e406f4079406060201010d0e8506a5ff34001200a3ff39002b0010100f00dc0fe40fdb0fdd0ff00f00000000e7")
 	tHelper.conn.TestHelper(0, int(len(decoded)), decoded)
 	log.Println("Bye")
-}
-
-// go test -v -run Test_Run
-func Test_Run(t *testing.T) {
-
-	log.Println("Hello!")
-
-	waitInstance := sync.WaitGroup{}
-
-	tHelper := testHelper{}
-	tHelper.conn = tortuga.Connection{}
-	err := tHelper.conn.Init(&waitInstance, tHelper.handler, "ttyUSB0")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	waitInstance.Add(1)
-	go tHelper.conn.Run()
-
-	for i := 0; i < 5; i++ {
-		log.Println(i)
-		tHelper.conn.Send(command.BaseControlCommand(50, 0))
-		time.Sleep(time.Second * 1)
-	}
-
-	tHelper.conn.Send(command.BaseControlCommand(0, 0))
-	time.Sleep(time.Second * 2)
-
-	for i := 0; i < 5; i++ {
-		log.Println(i)
-		tHelper.conn.Send(command.BaseControlCommand(-50, 0))
-		time.Sleep(time.Second * 1)
-	}
-
-	tHelper.conn.Send(command.BaseControlCommand(0, 0))
-
-	tHelper.conn.Stop()
-
-	waitInstance.Wait()
-
-	log.Println("Bye!")
 }
 
 func (t *testHelper) handler(feedback model.Feedback) {

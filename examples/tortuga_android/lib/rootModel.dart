f5b26ed278fa9621043097a488ef4cada9f780fe
bus710 <bus710@gmail.com>
2019-09-27 15:08:16 -0700

    wip

--- examples/tortuga_android/lib/rootModel.dart
+++ examples/tortuga_android/lib/rootModel.dart
@@ -1,45 +1,113 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
+import 'dart:convert'; // for JSON/struct conversion
+
+import 'package:flutter/material.dart';
+import 'package:universal_html/html.dart' as html;
 
 enum Status {
   init,
-  dial,
+  connecting,
   connected,
   disconnected,
+}
+
+enum Request {
+  dial,
+  connect,
+  send,
+  disconnect,
 }
 
 class RootModel with ChangeNotifier {
   Timer _timer;
   Status _state;
+  html.WebSocket _socket;
+  String _host;
+  String _buttonName;
 
   RootModel() {
-    _timer = Timer.periodic(Duration(milliseconds: 300), timerHandler);
+    _timer = Timer.periodic(Duration(milliseconds: 1000), timerHandler);
     _state = Status.init;
+    _buttonName = "none/none";
   }
 
   getStatus() => _state;
+  getSocketStatus() => _socket.readyState;
 
   void timerHandler(Timer timer) async {
-    // notifyListeners();
+    send();
   }
 
-  void pressHandler(Status request, String param) {
+  void pressHandler(Request request, String param) {
+    debugPrint(_state.toString() + " / " + request.toString() + " / " + param);
+    debugPrint(">>> " + _socket.readyState.toString());
     switch (_state) {
       case Status.init:
-        if (request == Status.dial) {
+        if (request == Request.dial) {
           // TODO: check the format of IP address
-          _state = Status.dial;
+          _host = param;
+          socketInit();
+          _state = Status.connecting;
         }
         break;
-      case Status.dial:
+      case Status.connecting:
         break;
       case Status.connected:
+        if (request == Request.send) {
+          // TODO: check the format of button name
+          _buttonName = param;
+          send();
+        }
         break;
       case Status.disconnected:
+        _buttonName = "none/none";
+        send();
+        _socket.close();
+        _state = Status.init;
         break;
       default:
         break;
     }
     notifyListeners();
+  }
+
+  void socketInit() {
+
+    if (_socket.readyState == html.WebSocket.OPEN) {
+      _socket.close();
+    }
+
+    debugPrint(">>> 1");
+
+    _socket = html.WebSocket('ws://' + _host + ':3000/message');
+
+    debugPrint(">>> 2");
+
+    _socket.onOpen.listen((e) async {
+      debugPrint("websocket: opened");
+      await Future.delayed(Duration(seconds: 2));
+      _state = Status.connected;
+      notifyListeners();
+    });
+
+    _socket.onClose.listen((e) {
+      debugPrint("websocket: closed");
+      _state = Status.disconnected;
+      notifyListeners();
+    });
+
+    _socket.onMessage.listen((e) {
+      // Do nothing
+    });
+  }
+
+  void send() {
+    if (_state == Status.connected &&
+        _socket != null &&
+        _socket.readyState == html.WebSocket.OPEN) {
+      _socket.send(json.encode({
+        "ButtonName": _buttonName,
+      }));
+    }
   }
 }
--- examples/tortuga_android/lib/rootPage.dart
+++ examples/tortuga_android/lib/rootPage.dart
@@ -22,15 +22,264 @@ class RootPage extends StatelessWidget {
         return InitPage(data: data);
         break;
       case Status.connected:
-        return InitPage(data: data);
+        return ConnectedPage(data: data);
         break;
-      case Status.dial:
+      case Status.connecting:
         return DialPage(data: data);
         break;
       case Status.init:
       default:
         return InitPage(data: data);
     }
+  }
+}
+
+class ConnectedPage extends StatefulWidget {
+  final AppData data;
+
+  ConnectedPage({Key key, this.data}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() {
+    return ConnectedPageState();
+  }
+}
+
+class BoxSize {
+  double height;
+  double width;
+
+  BoxSize({this.height, this.width});
+}
+
+class ConnectedPageState extends State<ConnectedPage> {
+  var rootModel;
+  Timer _timer;
+
+  BoxSize _mxSize; // the size of the entire box
+  BoxSize _fbSize; // the size of the feedback box
+  BoxSize _ctSize; // the size of the controller box
+
+  Color _blinkColor;
+  // Map<String, bool> _buttonState;
+  List<ButtonData> _buttonDataList;
+
+  @override
+  void initState() {
+    _timer = Timer.periodic(Duration(milliseconds: 500), timerCallback);
+
+    _mxSize = BoxSize(height: 0, width: 0);
+    _fbSize = BoxSize(height: 0, width: 0);
+    _ctSize = BoxSize(height: 0, width: 0);
+    _blinkColor = Colors.black;
+    // _buttonState = Map<String, bool>();
+    _buttonDataList = List<ButtonData>();
+
+    _buttonDataList
+        .add(ButtonData("forward/left", 20, 20, false, this.buttonCallback));
+    _buttonDataList
+        .add(ButtonData("forward/none", 110, 20, false, this.buttonCallback));
+    _buttonDataList
+        .add(ButtonData("forward/right", 200, 20, false, this.buttonCallback));
+
+    _buttonDataList
+        .add(ButtonData("none/left", 20, 110, false, this.buttonCallback));
+    _buttonDataList
+        .add(ButtonData("none/none", 110, 110, false, this.buttonCallback));
+    _buttonDataList
+        .add(ButtonData("none/right", 200, 110, false, this.buttonCallback));
+
+    _buttonDataList
+        .add(ButtonData("backward.left", 20, 200, false, this.buttonCallback));
+    _buttonDataList
+        .add(ButtonData("backward/none", 110, 200, false, this.buttonCallback));
+    _buttonDataList.add(
+        ButtonData("backward/right", 200, 200, false, this.buttonCallback));
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    rootModel = Provider.of<RootModel>(context);
+
+    return Center(
+      child: LayoutBuilder(builder: (context, constraint) {
+        _mxSize.height = constraint.maxWidth;
+        _mxSize.width = constraint.maxHeight;
+        // print(maxW.toString() + " / " + maxH.toString());
+        return getInterface();
+      }),
+    );
+  }
+
+  void buttonCallback(String pressedButtonName) {
+    // To the button indicate the color immediately
+    _timer.cancel();
+    _timer = Timer.periodic(Duration(milliseconds: 500), timerCallback);
+
+    // Only the button pressed last time should be set
+    // so that this handler clears everyone and sets the last one.
+    _buttonDataList.forEach((b) => {
+          b.clearButtonState(),
+          b.setBlinkState(),
+          if (b._name == pressedButtonName) {b.setButtonState()}
+        });
+
+    rootModel.pressHandler.send(Request.send, pressedButtonName);
+
+    setState(() {});
+  }
+
+  void timerCallback(Timer timer) {
+    // To blink the button pressed
+    _buttonDataList.forEach((b) => {
+          b.flipBlinkState(),
+        });
+    setState(() {});
+  }
+
+  Widget getInterface() {
+    if (_mxSize.width > 319) {
+      _fbSize.width = _ctSize.height = _ctSize.width = 319;
+      return Container(
+          decoration: BoxDecoration(
+            color: Color(widget.data.primaryColor),
+          ),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                _getTitleInterface(),
+                _getControllerInterface(),
+              ]));
+    } else {
+      return Text("The space available is too small");
+    }
+  }
+
+  Widget _getTitleInterface() {
+    double targetH;
+
+    if (_mxSize.height < 700) {
+      targetH = _mxSize.height - _ctSize.height - 30;
+    } else {
+      targetH = 340;
+    }
+
+    return Container(
+      width: _mxSize.width - 10,
+      height: targetH,
+      margin: EdgeInsets.only(left: 10, top: 20, right: 10, bottom: 10),
+      decoration: BoxDecoration(
+        color: Color(widget.data.primaryColor),
+      ),
+      child: Center(
+          child: Text(
+        "TORTUGA",
+        style: TextStyle(
+          fontSize: 48,
+          fontWeight: FontWeight.w100,
+          color: Colors.grey[200],
+        ),
+      )),
+    );
+  }
+
+  Widget _getControllerInterface() {
+    return Container(
+      width: _ctSize.width - 30,
+      height: _ctSize.height - 30,
+      margin: EdgeInsets.all(5),
+      decoration: BoxDecoration(
+        color: Color(widget.data.primaryColor),
+        /* boundary for debugging purpose */
+        // border: Border.all(color: Colors.grey[900], width: 3),
+        // borderRadius: BorderRadius.all(Radius.circular(5)),
+      ),
+      child: Stack(
+        children: List.generate(_buttonDataList.length, (index) {
+          return getButton(index);
+        }),
+      ),
+    );
+  }
+
+  // Widget _getButton(String name, double x, double y, bool state) {
+  Widget getButton(int index) {
+    return _buttonDataList[index].getButton();
+  }
+}
+
+class ButtonData {
+  String _name;
+  double _x, _y;
+  bool _state;
+  bool _blinkState;
+  Color _blinkColor;
+  Function _callback;
+
+  ButtonData(String name, double x, double y, bool state, Function callback) {
+    _name = name;
+    _x = x;
+    _y = y;
+    _state = state;
+    _blinkState = false;
+    _blinkColor = Colors.black;
+    _callback = callback;
+  }
+
+  Widget getButton() {
+    Color stateColor = Colors.white;
+    if (_state && _blinkState) {
+      stateColor = _blinkColor;
+    } else {
+      stateColor = Colors.grey[200];
+    }
+
+    return Positioned(
+      left: _x,
+      top: _y,
+      child: GestureDetector(
+          child: Container(
+            width: 70,
+            height: 70,
+            decoration: BoxDecoration(
+              color: stateColor,
+              border: Border.all(color: stateColor, width: 3),
+              borderRadius: BorderRadius.all(Radius.circular(5)),
+            ),
+          ),
+          onTap: () {
+            if (_state) {
+              _state = false;
+            } else {
+              _state = true;
+            }
+            // print("${name}");
+            _callback(_name);
+          }),
+    );
+  }
+
+  void clearButtonState() {
+    _state = false;
+  }
+
+  void setButtonState() {
+    _state = true;
+  }
+
+  void flipBlinkState() {
+    _blinkState = !_blinkState;
+  }
+
+  void setBlinkState() {
+    _blinkState = true;
+  }
+
+  void setBlinkColor(Color color) {
+    _blinkColor = color;
   }
 }
 
@@ -142,6 +391,7 @@ class InitPageState extends State<InitPage> {
   @override
   Widget build(BuildContext context) {
     var rootModel = Provider.of<RootModel>(context);
+
     return Scaffold(
       // Give 0 to remove appBar from the actual screen
       appBar: PreferredSize(
@@ -194,7 +444,7 @@ class InitPageState extends State<InitPage> {
                         borderRadius: BorderRadius.circular(5)),
                   ),
                   onFieldSubmitted: (address) {
-                    rootModel.pressHandler(Status.dial, address);
+                    rootModel.pressHandler(Request.dial, address);
                   },
                 ),
               ),
